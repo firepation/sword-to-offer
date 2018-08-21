0150710abcb4668f3ec53a2892d8609d460ecd57
firepation <firepation@gmail.com>
2018-08-21 11:13:13 +0800

    正则表达式匹配

--- src/B19RegulaMatch.java
+++ src/B19RegulaMatch.java
@@ -4,8 +4,7 @@ public class B19RegulaMatch {
 	 * 字符串 '.' 表示任意一个字符，而 '*' 表示它前面的字符可以出现任意次（含0次）。
 	 * 在本题中，匹配是指字符串的所有字符匹配整个模式。例如，字符串 "aaa"
 	 * 与模式  "a.a" 和 "ab*ac*a" 匹配，但与 "aa.a" 和 "ab*a" 均不匹配。
-	 */
-	
+	 */	
 	public boolean isMatch(String str,String pattern){
 		if(str==null || pattern==null)
 			return false;
