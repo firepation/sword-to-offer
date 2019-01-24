65385801ac5745cf813d015c303642327f3ed774
firepation <firepation@gmail.com>
2019-01-24 22:07:01 +0800

    简单排序算法及对数器的使用

--- newcode/sort/BubbleSort.java
+++ newcode/sort/BubbleSort.java
@@ -5,18 +5,21 @@ package newcode.sort;
  */
 public class BubbleSort {
 	public static void bubbleSort(int[] arr) {
-		if(arr == null || arr.length < 2) {
-			return ;
+		if (arr == null || arr.length < 2) {
+			return;
 		}
-		
-		for(int end = arr.length-1;end > 0;end--) {
-			
+		for (int e = arr.length - 1; e > 0; e--) {
+			for (int i = 0; i < e; i++) {
+				if (arr[i] > arr[i + 1]) {
+					swap(arr, i, i + 1);
+				}
+			}
 		}
 	}
-	
-	public static void swap(int[] arr,int i,int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
+
+	public static void swap(int[] arr, int i, int j) {
+		arr[i] = arr[i] ^ arr[j];
+		arr[j] = arr[i] ^ arr[j];
+		arr[i] = arr[i] ^ arr[j];
 	}
 }
